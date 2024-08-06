877de9f575de747c2e1304885c803549afab3c94
Kenny Baltazar alanoca <kenny@iMac-de-Kenny.local>
2024-08-06 15:53:21 -0500

    20%

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,10 @@
-// #include <Arduino.h>
-// #include <BLEDevice.h>
-// #include <BLEServer.h>
-// #include <BLECharacteristic.h>
-
-// const int buttonPin = 4;  // Pin del botón (GPIO04)
-// const int ledPin = 2;     // Pin del LED
-
-// void setup() {
-//   Serial.begin(9600);
-//   pinMode(buttonPin, INPUT_PULLDOWN);
-//   pinMode(ledPin, OUTPUT);
-// }
-
-// void loop() {
-//   // Lee el estado del botón
-//   int buttonState = digitalRead(buttonPin);
-//   Serial.println(buttonState);
-
-//   if (buttonState == HIGH) {
-//     Serial.println("Click in button");
-//     digitalWrite(ledPin, HIGH);  // Enciende el LED
-//     delay(2000);
-//   } else {
-//     digitalWrite(ledPin, LOW);   // Apaga el LED
-//   }
-//   delay(100);
-// }
-
 #include <Arduino.h>
 #include <BLEDevice.h>
-#include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLECharacteristic.h>
 
+const int buttonPin = 4;  // Pin del botón (GPIO04)
+const int ledPin = 2;     // Pin del LED
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define PUSH_ONE_PIN        4
@@ -53,36 +26,21 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 
 void setup() {
   Serial.begin(9600);
-
-  BLEDevice::init("ESP32 BLE example");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-
-  pCharacteristic->setCallbacks(new MyCallbacks());
-
-  pCharacteristic->setValue("Hello World");
-  pService->start();
-
-  BLEAdvertising *pAdvertising = pServer->getAdvertising();
-  pAdvertising->start();
+  pinMode(buttonPin, INPUT_PULLDOWN);
+  pinMode(ledPin, OUTPUT);
 }
-int counter;
-char buffer[50];
 
 void loop() {
-  delay(100);
-  int buttonState = digitalRead(PUSH_ONE_PIN);
+  // Lee el estado del botón
+  int buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+
   if (buttonState == HIGH) {
-    counter += 1;
-    sprintf(buffer, "Button pressed %d", counter);
-    Serial.println(buffer);
-    pCharacteristic->setValue(buffer);
-    pCharacteristic->notify();
-    delay(1000);
+    Serial.println("Click in button");
+    digitalWrite(ledPin, HIGH);  // Enciende el LED
+    delay(2000);
+  } else {
+    digitalWrite(ledPin, LOW);   // Apaga el LED
   }
+  delay(100);
 }
